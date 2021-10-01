e99d913c5f3b9f344c055617fadfc621d61fe07e
Gonzalo Ovalle <gonzalo.ovalle01@gmail.com>
2021-10-01 16:22:09 -0500

    controllers exercise

---
+++ src/main/java/com/codeup/gonzaloblog/GonzaloblogApplication.java
@@ -0,0 +1,13 @@
+package com.codeup.gonzaloblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GonzaloblogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GonzaloblogApplication.class, args);
+    }
+
+}
