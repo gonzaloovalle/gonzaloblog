f2c06db5bfd8d6b927194280fbfe08f1caac457b
Gonzalo Ovalle <gonzalo.ovalle01@gmail.com>
2021-10-05 16:04:55 -0500

    added multiple dice rolls

--- src/main/java/com/codeup/gonzaloblog/DiceController.java
+++ src/main/java/com/codeup/gonzaloblog/DiceController.java
@@ -17,15 +17,21 @@ class DiceController {
     @GetMapping("/roll-dice/{guess}")
     public String rollDice(@PathVariable int guess, Model model) {
         String message;
-        int randomNum = ThreadLocalRandom.current().nextInt(1, 6 + 1);
+        int randomNum1 = ThreadLocalRandom.current().nextInt(1, 6 + 1);
+        int randomNum2 = ThreadLocalRandom.current().nextInt(1, 6 + 1);
+        int randomNum3 = ThreadLocalRandom.current().nextInt(1, 6 + 1);
+        int randomNum4 = ThreadLocalRandom.current().nextInt(1, 6 + 1);
 
-        if (guess == randomNum){
+        if (guess == randomNum1 || guess == randomNum2 || guess == randomNum3 || guess == randomNum4){
             message = "Yay, you guessed correctly!";
         } else {
             message = "Sorry, guess again";
         }
 
-        model.addAttribute("randomNumber", randomNum);
+        model.addAttribute("randomNumber1", randomNum1);
+        model.addAttribute("randomNumber2", randomNum2);
+        model.addAttribute("randomNumber3", randomNum3);
+        model.addAttribute("randomNumber4", randomNum4);
         model.addAttribute("guess", guess);
         model.addAttribute("message", message);
 
