fb79465b6f9b0b5d9fe8a9a328febff5067d10d4
Gonzalo Ovalle <gonzalo.ovalle01@gmail.com>
2021-10-05 15:59:15 -0500

    dice roller

---
+++ src/main/java/com/codeup/gonzaloblog/DiceController.java
@@ -0,0 +1,36 @@
+package com.codeup.gonzaloblog;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+@Controller
+class DiceController {
+    @GetMapping("/roll-dice")
+    public String showRollDice() {
+        return "roll-dice";
+    }
+
+    @GetMapping("/roll-dice/{guess}")
+    public String rollDice(@PathVariable int guess, Model model) {
+        String message;
+        int randomNum = ThreadLocalRandom.current().nextInt(1, 6 + 1);
+
+        if (guess == randomNum){
+            message = "Yay, you guessed correctly!";
+        } else {
+            message = "Sorry, guess again";
+        }
+
+        model.addAttribute("randomNumber", randomNum);
+        model.addAttribute("guess", guess);
+        model.addAttribute("message", message);
+
+        return "roll-results";
+    }
+
+
+}
