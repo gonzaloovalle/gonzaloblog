b240678bea6ad2288127ed8d80be701c73a591a3
Gonzalo Ovalle <gonzalo.ovalle01@gmail.com>
2021-11-12 14:35:07 -0600

    completed security exercises

--- src/main/java/com/codeup/gonzaloblog/services/UserDetailsLoader.java
+++ src/main/java/com/codeup/gonzaloblog/services/UserDetailsLoader.java
@@ -10,15 +10,15 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class UserDetailsLoader implements UserDetailsService {
-    private final User users;
+    private final UserRepository userDao;
 
-    public UserDetailsLoader(User users) {
-        this.users = users;
+    public UserDetailsLoader(UserRepository userDao) {
+        this.userDao = userDao;
     }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = users.findByUsername(username);
+        User user = userDao.findByUsername(username);
         if(user == null) {
             throw new UsernameNotFoundException("No user found for " + username);
         }
